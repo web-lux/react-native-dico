7a892fa7536ddaa206474563b05f6069e88d4e1e
web-lux <web-lux@outlook.fr>
2024-02-27 15:22:13 +0100

    create placeholder data

---
+++ assets/placeholder.js
@@ -0,0 +1,68 @@
+export const placeholder = {
+	category: "nom féminin",
+	definitions: [
+		{
+			value: "Meuble sur pied(s) comportant une surface plane.",
+			examples: [
+				"Table ronde ; à rallonges.",
+				"Table basse.",
+				"Table roulante.",
+			],
+		},
+		{
+			value: "Le meuble où l'on prend ses repas.",
+			examples: [],
+			context: "spécialement",
+		},
+		{
+			value: "La nourriture.",
+			examples: ["Les plaisirs de la table."],
+		},
+		{
+			value: "Ceux qui prennent leur repas, qui sont à table.",
+			examples: ["Présider la table."],
+		},
+		{
+			value: "Table servant à d'autres usages que les repas.",
+			examples: ["Table de travail.", "Table à dessin."],
+		},
+		{
+			value: "Réunion pour discuter d'un problème.",
+			examples: ["Participer à une table ronde."],
+		},
+		{
+			value:
+				"Meuble comprenant, outre un support plat, différentes parties (tiroirs, tablettes…).",
+			examples: [
+				"Table de nuit, de chevet : petit meuble placé au chevet du lit.",
+			],
+		},
+		{
+			value: "Partie supérieure de l'autel.",
+			examples: ["La sainte table, l'autel."],
+		},
+		{
+			value:
+				"Partie plane ou légèrement incurvée d'un instrument de musique, sur laquelle les cordes sont tendues.",
+			examples: ["Table (d'harmonie), sur laquelle repose le chevalet."],
+		},
+		{
+			value: "Surface plane naturelle.",
+			examples: ["Une table calcaire."],
+		},
+		{
+			value: "Surface plane sur laquelle on peut écrire, inscrire.",
+			examples: [],
+			context: "(dans quelques emplois)",
+		},
+		{
+			value: "Présentation méthodique sous forme de liste ou de tableau.",
+			examples: ["Table alphabétique."],
+		},
+		{
+			value:
+				"Recueil d'informations, de données groupées de façon systématique.",
+			examples: ["Tables de multiplication."],
+		},
+	],
+};
