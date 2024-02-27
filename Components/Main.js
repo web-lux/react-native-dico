6112deeb1a13db18a1402ed97cc92facb8c51236
web-lux <web-lux@outlook.fr>
2024-02-27 15:22:25 +0100

    display definitions of placeholder data

--- Components/Main.js
+++ Components/Main.js
@@ -1,18 +1,33 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, FlatList } from "react-native";
+import { placeholder } from "../assets/placeholder";
 
 export default function Main() {
-    return (
-        <View style={styles.mainWrapper}>
-            <Text>Lorem Ipsum</Text>
-        </View>
-    );
-};
+	return (
+		<View style={styles.mainWrapper}>
+			<Text>Lorem Ipsum</Text>
+			<Text>{placeholder.category}</Text>
+
+			<FlatList
+				data={placeholder.definitions}
+				renderItem={(definition) => (
+					<View>
+						<Text>
+							{definition.index + 1}. {definition.item.value}
+						</Text>
+						{definition.item.examples.length !== 0 && (
+							<Text>{definition.item.examples}</Text>
+						)}
+					</View>
+				)}></FlatList>
+		</View>
+	);
+}
 
 const styles = StyleSheet.create({
-    mainWrapper: {
-      flex: 1,
-      backgroundColor: "#F4F4FC",
-      borderTopRightRadius: 20,
-      borderTopLeftRadius: 20
-    },
+	mainWrapper: {
+		flex: 1,
+		backgroundColor: "#F4F4FC",
+		borderTopRightRadius: 20,
+		borderTopLeftRadius: 20,
+	},
 });
